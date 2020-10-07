445f32f60edbc179b56d8a9839a2746e3c1ba5cc
Alejandro Torres <alejandrojuegoselx@gmail.com>
2020-10-07 17:07:38 +0200

    [ADD] Created tracking service

--- app/src/main/java/com/example/mvvm_maps/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/mvvm_maps/ui/fragments/TrackingFragment.kt
@@ -1,15 +1,20 @@
 package com.example.mvvm_maps.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.mvvm_maps.R
+import com.example.mvvm_maps.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.example.mvvm_maps.services.TrackingService
 import com.example.mvvm_maps.ui.viewmodels.MainViewModel
 import com.example.mvvm_maps.ui.viewmodels.StatisticsViewModel
 import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
+import timber.log.Timber
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking){
@@ -21,12 +26,23 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        btnToggleRun.setOnClickListener{
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
+
         mapView.onCreate(savedInstanceState)
 
         mapView.getMapAsync {
                 map = it
         }
     }
+
+    private fun sendCommandToService(action:String) =
+        Intent(requireContext(), TrackingService::class.java).also {
+            it.action = action
+            requireContext().startService(it)
+        }
+
 
     override fun onResume() {
         super.onResume()
